ef1508f0789052d32f02ad8e62eb15ec30a4c5c8
guojing <guojing@weimi.me>
2014-11-25 18:40:59 +0800

    更新 update

--- db_conn.py
+++ db_conn.py
@@ -16,27 +16,30 @@ import mysql.connector
 
 class DBConn(object):
 
-	conn = None
-	cur = None
+    conn = None
+    cur = None
 
-	def __init__(self, user, password, database, host):
-		'''Init parameters'''
-		self.user = user
-		self.password = password
-		self.database = database
-		self.host = host
+    def __init__(self, user, password, database, host):
+        '''Init parameters'''
+        self.user = user
+        self.password = password
+        self.database = database
+        self.host = host
 
-	def query(self, sql, param):
-		'''select resultset from database'''
-		try:
-			conn = mysql.connector.connect(user=self.user, password=self.password, host=self.host, database=self.database)
-			cur = conn.cursor()
-			cur.execute(sql, (param,))
-			datas = cur.fetchall()
-			cur.close()
-			conn.close()
-			return datas
-		except Exception, e:
-			print e
-			return 
+    def query(self, sql, tuple_args):
+        '''select resultset from database'''
+        try:
+            conn = mysql.connector.connect(user=self.user, password=self.password, host=self.host, database=self.database)
+            cur = conn.cursor()
+            cur.execute(sql, tuple_args)
+            data = cur.fetchall()
+            cur.close()
+            conn.close()
+            return data
+        except Exception, e:
+            print e
+            return
+    def query_without_args(self, sql):
+        return self.query(sql, None)
+
 
--- operate_excel.py
+++ operate_excel.py
@@ -12,32 +12,55 @@ Require: 需要先安装Python模块: xlrd xlwt xlutils
 __author__ = 'guojing'
 
 from xlrd import open_workbook
-from xlwt import easyxf
+import xlwt
+#from xlwt import easyxf
 from xlutils.copy import copy
 
 class Excel(object):
 
-	#def __init__(self):
+    #def __init__(self):
 
+    def write(self, tpl, dest_file, *tuple_args):
+        # 单元格样式
+        style = xlwt.XFStyle()
+        font = xlwt.Font()
+        font.name = 'SimSun' #设置字体
+        style.font = font
+        tpl_book = open_workbook(tpl, formatting_info=True)
+        target = copy(tpl_book)
+        i = 0
+        for arg in tuple_args:
+            sheet = target.get_sheet(i) # a writable copy
+            i += 1
+            size = len(arg)
+            cols = len(arg[0])
+            for row in xrange(0,size):
+                for col in xrange(0, cols):
+                    sheet.write(row + 1, col, arg[row][col], style)
+        target.save(dest_file)
 
-	def write(self, datas, tpl_filename, save_path):
-		'''
-		Args:
-		  datas: 从数据库获取的结果集(list)
-		  save_path: 生成的excel文件名
-		  tpl_filename: excel模板
-		'''
-		tpl = open_workbook(tpl_filename, formatting_info=True)
-		tpl_sheet = tpl.sheet_by_index(0) #read only
-		nrows = tpl_sheet.nrows
-		ncols = tpl_sheet.ncols
-		target = copy(tpl)
-		sheet = target.get_sheet(0) # a writable copy
-		size = len(datas)
-		for row in xrange(0,size - 1):
-			for col in xrange(0, ncols):
-				sheet.write(row + 1, col, datas[row][col])
-		target.save(save_path)
+    # def write(self, tpl, dest_file, datas):
+    #     '''
+    #     Args:
+    #       datas: 从数据库获取的结果集(list)
+    #       dest_file: 生成的excel文件名
+    #       tpl: excel模板
+    #     '''
+    #     tpl_book = open_workbook(tpl, formatting_info=True)
+    #     tpl_sheet = tpl_book.sheet_by_index(0) #read only
+    #     nrows = tpl_sheet.nrows
+    #     ncols = tpl_sheet.ncols
+    #     target = copy(tpl_book)
+    #     sheet = target.get_sheet(0) # a writable copy
+    #     style = xlwt.XFStyle()
+    #     font = xlwt.Font()
+    #     font.name = 'SimSun' #设置字体
+    #     style.font = font
+    #     size = len(datas)
+    #     for row in xrange(0,size):
+    #         for col in xrange(0, ncols):
+    #             sheet.write(row + 1, col, datas[row][col], style)
+    #     target.save(dest_file)
 
-		
-	
+        
+    
